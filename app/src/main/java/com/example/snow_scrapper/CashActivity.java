61031c6f370eb1cdc4a746a6df4f82e40256e8a0
Vidhu Gaba <vidhurockstar.gaba@gmail.com>
2022-04-18 17:50:47 -0400

    add new button

--- app/src/main/java/com/example/snow_scrapper/CashActivity.java
+++ app/src/main/java/com/example/snow_scrapper/CashActivity.java
@@ -1,14 +1,32 @@
 package com.example.snow_scrapper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CashActivity extends AppCompatActivity {
 
+    private Button orderHis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cash);
+
+        orderHis = (Button) findViewById(R.id.orderHis);
+        orderHis.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                orderHistory();
+            }
+        });
+    }
+
+    public void orderHistory(){
+        Intent intent = new Intent(this, OrderDetailsActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/snow_scrapper/StatusActivity.java
+++ app/src/main/java/com/example/snow_scrapper/StatusActivity.java
@@ -2,10 +2,15 @@ package com.example.snow_scrapper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class StatusActivity extends AppCompatActivity {
+
+    private Button orderHis1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,18 @@ public class StatusActivity extends AppCompatActivity {
 
         ImageView iv = (ImageView) findViewById(R.id.imageView);
         iv.setImageResource(R.drawable.thankyou);
+
+        orderHis1 = (Button) findViewById(R.id.orderHist1);
+        orderHis1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                orderHistory1();
+            }
+        });
+    }
+
+    public void orderHistory1(){
+        Intent intent = new Intent(this, OrderDetailsActivity.class);
+        startActivity(intent);
     }
 }
